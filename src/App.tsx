cd9ba0aef4a7d7eb01a42c45ff873764d089bab6
fredafei <964959033@qq.com>
2022-12-19 20:42:38 +0800

    初始化项目

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      hi, {count}
+      <button onClick={()=>setCount(v=>v+1)}>+1</button>
+    </div>
+  )
+}
+
+export default App
