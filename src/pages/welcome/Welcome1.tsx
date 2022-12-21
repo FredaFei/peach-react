0ea79d370193487c2078d9818186041b0950dfb6
fredafei <964959033@qq.com>
2022-12-21 21:20:34 +0800

    配置unocss

--- src/pages/welcome/Welcome1.tsx
+++ src/pages/welcome/Welcome1.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react'
-import { NavLink } from 'react-router-dom'
 export const Welcome1: React.FC = () => {
   return (
-    <div style={{ border: '1px solid red' }}> 1 9<NavLink to="/welcome/2">下一页</NavLink> </div>
+    <div flex justify-center items-center
+      after="content-[hi] b-1 b-red"
+      before="content-[hi] b-1 b-red"
+      shadow
+    >
+      <header hover:bg-red w-100px h-40px b-1 b-green-500></header>
+      <main grow-1 b-1 b-blue h-100px></main>
+      <footer w-200px b-4 b-black h-55px></footer>
+    </div>
   )
 }
