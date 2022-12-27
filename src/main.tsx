a2eea5a0eba3e3eff1a9b4b7f8779b67496a00b1
fredafei <964959033@qq.com>
2022-12-27 10:02:32 +0800

    添加sass等样式依赖、全局样式文件

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, } from 'react-router-dom'
 import { router } from './routes/router'
 import 'virtual:uno.css'
+import './global.scss'
 
 const el = document.getElementById('root') as HTMLElement
 ReactDOM.createRoot(el).render(
