0658e6c1c616b91da7c1586d7fd0ad44f0158b3d
fredafei <964959033@qq.com>
2022-12-27 12:12:27 +0800

    调整route配置

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,28 +1,23 @@
 import { createBrowserRouter, } from 'react-router-dom'
-import { NotFound } from '../pages/NotFound'
-import { MainLayout } from '../layouts/MainLayout'
 import { WelcomeLayout } from '../layouts/WelcomeLayout'
 import { Welcome1 } from '../pages/welcome/Welcome1'
 import { Welcome2 } from '../pages/welcome/Welcome2'
 import { Welcome3 } from '../pages/welcome/Welcome3'
 import { Welcome4 } from '../pages/welcome/Welcome4'
+import { Root } from '../components/Root'
+import { Home } from '../pages/Home'
 
 export const router = createBrowserRouter([
+  { path: '/',element: <Root /> },
+  { path: '/home',element: <Home /> },
   {
-    path: '/',
-    element: <MainLayout />,
-    errorElement: <NotFound />,
+    path: '/welcome',
+    element: <WelcomeLayout />,
     children: [
-      {
-        path: 'welcome',
-        element: <WelcomeLayout />,
-        children: [
-          { path: '1', element: <Welcome1 /> },
-          { path: '2', element: <Welcome2 /> },
-          { path: '3', element: <Welcome3 /> },
-          { path: '4', element: <Welcome4 /> },
-        ]
-      }
+      { path: '1', element: <Welcome1 /> },
+      { path: '2', element: <Welcome2 /> },
+      { path: '3', element: <Welcome3 /> },
+      { path: '4', element: <Welcome4 /> },
     ],
   },
 ])
