54331b3fd0cbc8e0864f44e261cf798c72211c57
fredafei <964959033@qq.com>
2023-01-01 18:45:19 +0800

    添加 vite-plugin-mock mock数据

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Unocss from 'unocss/vite'
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -11,6 +12,7 @@ export default defineConfig(({ command }) => {
     plugins: [
       Unocss(),
       react(),
+      viteMockServe()
     ],
   }
 })
