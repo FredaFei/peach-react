86f407074f7928fa2ef14c8744f79f7c7ec13597
fredafei <964959033@qq.com>
2023-01-01 18:28:56 +0800

    封装ajax，添加生成和开发的路径配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react'
 import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    Unocss(),
-    react(),
-  ],
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      isDev: command === 'serve'
+    },
+    plugins: [
+      Unocss(),
+      react(),
+    ],
+  }
 })
