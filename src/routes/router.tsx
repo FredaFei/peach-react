04fba48ca5b609905d14a6f7a424252a72a97772
fredafei <964959033@qq.com>
2023-01-01 18:46:27 +0800

    添加mock，ajax 接受泛型

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -6,6 +6,7 @@ import { Welcome3 } from '../pages/welcome/Welcome3'
 import { Welcome4 } from '../pages/welcome/Welcome4'
 import { Root } from '../components/Root'
 import { Home } from '../pages/Home'
+import { Items } from '../pages/Items'
 
 export const router = createBrowserRouter([
   { path: '/',element: <Root /> },
@@ -20,4 +21,5 @@ export const router = createBrowserRouter([
       { path: '4', element: <Welcome4 /> },
     ],
   },
+  { path: '/items',element: <Items /> },
 ])
